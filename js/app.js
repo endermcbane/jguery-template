bc46458e58180e11fb4954b92e90464b26d7d1f8
endermcbane <kccrookston@gmail.com>
2014-09-18 19:12:40 -0600

    after lots of changes

--- js/app.js
+++ js/app.js
@@ -1,62 +1,66 @@
 $(document).ready(function(){
-	 $('.ryu').mouseenter(function() {
-	    $('.ryu-still').hide();
-		$('.ryu-ready').show();
-	  })
-	 .mouseleave(function() {
-	    $('.ryu-still').show();
-		$('.ryu-ready').hide();
-	  })
-	 .on('mousedown', function() {
+	$("#add").click (function(){
+		var my_text = $("#entry").val();
+		var my_item = $('<div class="food"></div>').text(my_text); addCheckbox();
+		
 
-	 	$('.ryu-ready').hide();
-	 	$('.ryu-throwing').show();
-	 	$('.hadouken').finish().show().animate(
-	  {'left': '300px'},
-	  500,
-	  function() {
-	    $(this).hide();
-	    $(this).css('left', '-212px');
-	  });
-	 
-	 })
-	 .on('mouseup', function() {
-	 	$('.ryu-throwing').hide();
-	 	$('.ryu-ready').show();
-	 	$('.hadouken').hide();
-	 })
+		my_item.on('click', '.cbox', function(){
+			$(this).closest('.food').toggleClass("checked_food");
+		});
 
-	})
+		my_item.on('click', ".lite-x-red", function(){
+			$(this).closest('.food').fadeOut('fast');
+		});
 
-$( ".main" ).keydown(function( event ) {
-  if ( event.which == 88 ) {
-   $('.ryu-still').hide();
-	  	
-	    $('.ryu-cool').show();
-  }
-})
+		if (my_text!=""){
+			$("#inner").append(my_item);
+			$("#entry").val("");
+		}
 
+		else{alert("Type an item in before you add it.")}
 
-/*
-	.keydown(function (e){
-	  if (e.keyCode == 88) {
-	  	$('.ryu-still').hide();
-	  	$('.ryu-ready').hide();
-	    $('.ryu-cool').show();
-	   }
-	   .keyup(function (e){
-	  if (e.keyCode == 88) {
-	  	$('.ryu-still').show();
-	  	$('.ryu-ready').hide();
-	    $('.ryu-cool').hide();
-	   }
-	})
+	function addCheckbox() {
+
+   $('<input />', { type: 'checkbox', class: 'food', class: "cbox"}).appendTo(my_item);
+   $('<input />', { type: 'image', class: "lite-x-red"}).appendTo(my_item);
+   
+}
+	if (my_text.length > 28) {
+		alert("seriously, that is the longest dumbest name to put on a shopping list. Abreviate!")
+
+	}
 	});
-*/
+  
+  $('#inner').keypress(function(e){
+    if(e.which == 13){
+    	var my_text = $("#entry").val();
+    	var my_item = $('<div class="food"></div>').text(my_text); addCheckbox();
 
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-};
+			my_item.on('click', '.cbox', function(){
+			$(this).closest('.food').toggleClass("checked_food");
+		});
+
+			my_item.on('click', ".lite-x-red", function(){
+			$(this).closest('.food').fadeOut('fast');
+		});
+
+			if (my_text!="")
+			{
+				$("#content").append(my_item);
+				$("#entry").val("");
+			}
+
+			else 
+			{
+				alert("Type an item in before you add it.")
+			}
+			function addCheckbox() {
+
+   $('<input />', { type: 'checkbox', class: 'food', class: "cbox"}).appendTo(my_item);
+   $('<input />', { type: 'image', class: "lite-x-red"}).appendTo(my_item);
+   
+}
+    }
+	});
+});
